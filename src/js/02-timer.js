95278d4d060361ea460b8c70411a22a27400d225
Andrii Popov <popov.andrey80@gmail.com>
2022-10-09 00:28:21 +0300

    start task-02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,52 @@
-// import flatpickr from 'flatpickr';
-// import 'flatpickr/dist/flatpickr.min.css';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-// const refs = {};
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+const options = {
+  enableTime: true, // Enables time picker
+  time_24hr: true, // Displays time picker in 24 hour mode without AM/PM selection when enabled.
+  defaultDate: new Date(), // Sets the initial selected date(s).
+  minuteIncrement: 1, // Adjusts the step for the minute input (incl. scrolling)
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+refs.input.addEventListener('select', () => {
+  console.log('hello world');
+});
+
+function flatpickr(selector, options) {
+  console.log('hello world');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
